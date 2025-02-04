5d24f6bfdebcf94ba057260ee1aa71c423000df3
eited <735408478mm@gmail.com>
2025-02-04 03:20:59 +0300

    qwertster

--- LAHJA/Data/UI/Components/DataBuildPlansBase.cs
+++ LAHJA/Data/UI/Components/DataBuildPlansBase.cs
@@ -73,6 +73,15 @@ namespace LAHJA.Data.UI.Components
         public bool IsGpu { get; set; }
         public bool IsGlobal { get; set; }
         public int Bandwidth { get; set; }
+
+        public RenderFragment Dash { get; set; } = builder => builder.AddContent(0, (RenderFragment)(b =>
+            {
+                b.OpenComponent<MudButton>(0);
+                b.AddAttribute(1, "Variant", Variant.Text);
+                b.AddAttribute(2, "StartIcon", Icons.Material.Filled.Person);
+                b.AddAttribute(3, "ChildContent", (RenderFragment)(c => c.AddContent(0, "الملف الشخصي")));
+                b.CloseComponent();
+            }));
     }
 
     public class AccessTokenAuth
